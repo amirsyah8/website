448e526fa5a6cad727930e265c00f27f872ff4c6
amirsyah8 <amirsyah87@gmail.com>
2024-11-15 00:39:25 +0800

    1a st commit

--- index.js
+++
@@ -1,6 +0,0 @@
-let userName;
-
-document.getElementById("bName").onclick = function(){
-    userName = document.getElementById("myName").value;
-    document.getElementById("myH1").textContent = `Hello ${userName}`;
-}
